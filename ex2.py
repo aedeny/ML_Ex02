0cd6840d718567f737b080788252889a098d1ef0
Eden Yefet <edenyefet@gmail.com>
2018-04-17 13:43:31 +0300

    fixes

--- ex2.py
+++ ex2.py
@@ -1,6 +1,9 @@
 import sys
 
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as sc
+from matplotlib.legend_handler import HandlerLine2D
 
 
 def softmax(z):
@@ -18,43 +21,64 @@ def sgd(params, learning_rate):
 
 
 class LogisticRegression(object):
-    def __init__(self, input, label, n_in, n_out):
-        self.x = input
+    def __init__(self, in_data, label, n_in, n_out):
+        self.x = in_data
         self.y = label
         self.w = np.zeros((n_in, n_out))  # initialize w 0
-        self.b = np.zeros(n_out)  # initialize bias 0
-        self.y_hat = None
+        self.b = np.zeros((n_in, n_out))  # initialize bias 0
 
-    def train(self, lr=0.1, input=None, l2_reg=0.00):
-        if input is not None:
+    def train(self, num_of_epochs=30, lr=0.1, in_data=None):
+        if in_data is not None:
             self.x = input
 
-        # Shuffles arrays
-        s = np.arange(self.x.shape[0])
-        np.random.shuffle(s)
-        self.x = self.x[s]
-        self.y = self.y[s]
+        for epoch in range(num_of_epochs):
 
-        for x, y in zip(self.x, self.y):
-            z = np.dot(x, self.w) + self.b
-            self.y_hat = np.argmax(softmax(z)) + 1
-            if y != self.y_hat:
-                for i in range(1, 4):
-                    if i == y:
-                        loss_dw = softmax(z) * x - x
-                        loss_db = softmax(z) - 1
-                    else:
-                        loss_dw = softmax(z) * x
-                        loss_db = softmax(z)
+            # Shuffles arrays
+            s = np.arange(self.x.shape[0])
+            np.random.shuffle(s)
+            self.x = self.x[s]
+            self.y = self.y[s]
 
-                    self.w[i-1] -= lr * loss_dw
-                    self.b[i-1] -= lr * loss_db
+            for x, y in zip(self.x, self.y):
+                z = np.dot(x, self.w) + self.b
 
-    def negative_log_likelihood(self):
-        return - np.sum(self.y * np.log(self.y_hat))
+                # Predicts
+                y_hat = np.argmax(softmax(z)) + 1
 
-    def predict(self, x):
-        return softmax(np.dot(x, self.w) + self.b)
+                current_softmax = softmax(z)
+                if y != y_hat:
+                    for a in range(3):
+                        if a + 1 == y:
+                            self.w[0, a] -= lr * (current_softmax[0, a] * x - x)
+                            self.b[0, a] -= lr * (current_softmax[0, a] - 1)
+                        else:
+                            self.w[0, a] -= lr * current_softmax[0, a] * x
+                            self.b[0, a] -= lr * current_softmax[0, a]
+
+    def draw_plot(self):
+        x_values = np.arange(0, 10, 0.1).tolist()
+        pdf_y_values = []
+        model_y_values = []
+        for val in x_values:
+            pdf_y_values.append(sc.norm(2, 1).pdf(val) / (sc.norm(2, 1).pdf(val) + sc.norm(4, 1).pdf(val) +
+                                                          sc.norm(6, 1).pdf(val)))
+            model_y_values.append(softmax(np.dot(self.w, val) + self.b).tolist()[0][0])
+
+        print(pdf_y_values)
+        print(model_y_values)
+        fig = plt.figure(0)
+        fig.canvas.set_window_title('normal distribution VS logistic regression')
+        plt.axis([0, 10, 0, 2])
+        plt.xlabel('X')
+        plt.ylabel('Probability')
+        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+        normal_graph, = plt.plot(x_values, pdf_y_values, 'r--', label="Normal Distribution")
+
+        plt.plot(x_values, model_y_values, label="Logistic Regression")
+
+        plt.legend(handler_map={normal_graph: HandlerLine2D(numpoints=4)})
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -67,19 +91,15 @@ if __name__ == '__main__':
             x = np.append(x, np.array([np.random.normal(2 * a, 1, 1)]), axis=0)
             y = np.append(y, np.array([[a]]), axis=0)
 
+    # # Draws a scatter of points
+    # plt.scatter(x[0:100], [1] * 100, color='green')
+    # plt.scatter(x[100:200], [2] * 100, color='red')
+    # plt.scatter(x[200:300], [3] * 100, color='blue')
+    # plt.show()
+
     # Constructs the Logistic Regression classifier
     classifier = LogisticRegression(x, y, 1, 3)
-
-    # Trains model
-    learning_rate = 0.01
-    n_epochs = 200
-
-    for epoch in xrange(n_epochs):
-        classifier.train(learning_rate)
-        cost = classifier.negative_log_likelihood()
-        print("Training epoch " + str(epoch) + " cost is " + str(cost))
-        learning_rate *= 0.95
-
+    classifier.train()
+    classifier.draw_plot()
     # Tests model
     x = np.array([3.23149])
-    print >> sys.stderr, classifier.predict(x)
